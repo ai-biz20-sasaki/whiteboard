2b0419965cc08bd3f998f796f9a3edc1246bdffe
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-18 16:02:29 +0900

    Header,Footer,Drawingコンポーネントを追加

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,56 +1,13 @@
-"use client"
-import { useState } from 'react';
+import Header from "@/components/header"
+import Drawing from "@/components/drawing"
+import Footer from "@/components/footer"
 
 export default function Top() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [trail, setTrail] = useState([]);
-
-  const handlePointerMove = ((e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-    setTrail((prevTrail) => [...prevTrail, { x: e.clientX, y: e.clientY }]);
-  });
-
   return (
-    <div
-      onPointerMove={handlePointerMove}
-      style={{
-        position: 'relative',
-        width: '100vw',
-        height: '100vh',
-      }}>
-
-      {trail.map((point, index) => {
-        if (index === 0) {
-          return null; // 最初の座標点は線を描画しない
-        }
-
-        const prevPoint = trail[index - 1];
-
-        return (
-          <div
-            key={index}
-            style={{
-              position: 'absolute',
-              backgroundColor: 'red',
-              width: '20px',
-              height: '20px',
-              transform: `translate(${prevPoint.x}px, ${prevPoint.y}px)`,
-              transition: 'transform 0.05s ease-out',
-            }}
-          />
-        );
-      })}
-
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'red',
-        borderRadius: '50%',
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        left: -10,
-        top: -10,
-        width: 20,
-        height: 20,
-      }} />
-    </div>
+    <>
+      <Header />
+      <Drawing />
+      <Footer />
+    </>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="bg-gray-900 text-white py-4 px-8">
+      <h1 className="text-2xl font-bold">whiteboad</h1>
+    </div>    
+  )
+}
