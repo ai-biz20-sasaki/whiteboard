823d1131f5018ab2eda92e22137cd6000743b409
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-18 13:57:00 +0900

    過去と現在の座標間の軌跡を表示する処理を追加

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -2,24 +2,45 @@
 import { useState } from 'react';
 
 export default function Top() {
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [trail, setTrail] = useState([]);
+
+  const handlePointerMove = ((e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+    setTrail((prevTrail) => [...prevTrail, { x: e.clientX, y: e.clientY }]);
   });
 
   return (
     <div
-      onPointerMove={e => {
-        setPosition({
-          x: e.clientX,
-          y: e.clientY
-        });
-      }}
+      onPointerMove={handlePointerMove}
       style={{
         position: 'relative',
         width: '100vw',
         height: '100vh',
       }}>
+
+      {trail.map((point, index) => {
+        if (index === 0) {
+          return null; // 最初の座標点は線を描画しない
+        }
+
+        const prevPoint = trail[index - 1];
+
+        return (
+          <div
+            key={index}
+            style={{
+              position: 'absolute',
+              backgroundColor: 'red',
+              width: '20px',
+              height: '20px',
+              transform: `translate(${prevPoint.x}px, ${prevPoint.y}px)`,
+              transition: 'transform 0.05s ease-out',
+            }}
+          />
+        );
+      })}
+
       <div style={{
         position: 'absolute',
         backgroundColor: 'red',
