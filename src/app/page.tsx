541f8159932d431b80cba86673e513421983529d
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-18 09:46:10 +0900

    TOPページを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-4xl font-bold">Whiteboard</div>
+      <Link
+        className="text-4xl font-bold"
+        href="/top"
+      >
+          Whiteboard
+      </Link>
       <div><br /></div>
       <div className="text-xl">Welcome my site</div>
     </div>
