0f5ccd06d230db17f11a7570849f83b41d2abdb7
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-21 11:01:04 +0900

    サイズサークルホバー時のスタイルを追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ export default function Header() {
     <div className="flex flex-row items-center bg-gray-900 text-white py-4 px-8">
       <h1 className="text-xl font-bold mr-3">Size</h1>
       <div className="flex flex-row items-center">
-        <button className="p-2">
+        <button className="p-2 hover:bg-blue-500">
           <IconCircle size={12} />
         </button>
-        <button className="p-2">
+        <button className="p-2 hover:bg-blue-500">
           <IconCircle size={24} />
         </button>
-        <button className="p-2">
+        <button className="p-2 hover:bg-blue-500">
           <IconCircle size={36} />
         </button>
       </div>
